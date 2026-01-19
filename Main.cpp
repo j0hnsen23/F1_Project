3f490edcb7690ab7ce17849306161b62542d234b
Nicolai Gravdal Johnsen <johnsennicolai05@gmail.com>
2026-01-19 10:29:33 +0100

    Drivers struct, pointsTable, Vectors and RNG

--- Main.cpp
+++ Main.cpp
@@ -3,3 +3,22 @@
 #include <random>           // mt19937, random_device
 #include <vector>           // Vector
 #include <string>           // String
+
+using namespace std;
+
+struct Driver{
+    string name;
+    string team;
+    int points;
+};
+
+int pointsTable[10] = {25,18,15,12,10,8,6,4,2,1};
+
+void simulateRace(
+    const vector<Driver>& Drivers,
+          vector<Driver>& Standings,
+          mt19937& RandomNumberGenerator
+
+){
+    
+}
